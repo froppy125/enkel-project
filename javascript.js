6b363a843e3505c5244d09addbf73f096579c857
froppy125 <dragonboyz11301@gmail.com>
2026-01-19 11:24:11 +0100

    test

--- javascript.js
+++ javascript.js
@@ -1,36 +1,9 @@
-const linje1 = document.getElementById("linje1")
-const linje2 = document.getElementById("linje2")
-const linje3 = document.getElementById("linje3")
-const linje4 = document.getElementById("linje4")
-const linje5 = document.getElementById("linje5")
-const linje6 = document.getElementById("linje6")
 
-function myfunction () {
-alert (linje1.innerText);
+
+
+function myfunction (linjeNumber) {
+    const linjeNumb = document.getElementById("linje" + linjeNumber);
+alert (linjeNumb.innerText);
 
 }
 
-function myfunction2() {
-alert (linje2.innerText);
-
-}
-
-function myfunction3() {
-alert (linje3.innerText);
-
-}
-
-function myfunction4() {
-alert (linje4.innerText);
-
-}
-
-function myfunction5() {
-alert (linje5.innerText);
-
-}
-
-function myfunction6() {
-alert (linje6.innerText);
-
-}
